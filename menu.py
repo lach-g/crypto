fc2c90ec127cc01991aa826daf9ca698b95b31a9
Lachlan Greenland <lachlan.greenland@gmail.com>
2021-01-11 14:15:04 +0800

    cleaned up menu direct

--- menu.py
+++ menu.py
@@ -60,24 +60,15 @@ class cryptoMenu:
         if choice == 1:
             self.loading_data_menu()
         elif choice == 2:
-            if self.current_market.has_asset_data() == False:
-                print("\n--LOAD ASSETS DATA FIRST--\n")
-            else:
-                self.asset_details_menu()
+            self.asset_details_menu()
         elif choice == 3:
-            if self.current_market.has_trades_data() == False:
-                print("\n--LOAD TRADES DATA FIRST--\n")
-            else:
-                self.trade_details_menu()
+            self.trade_details_menu()
         elif choice == 4:
             print("\nFinding trade paths")
         elif choice == 5:
-            print("\nSetting asset filter")
+            self.set_asset_filter()
         elif choice == 6:
-            if self.current_market.has_asset_data() == False:
-                print("\n--LOAD ASSETS DATA FIRST--\n")
-            else:
-                self.asset_overview()
+            self.asset_overview()
         elif choice == 7:
             self.trade_overview()
         elif choice == 8:
@@ -135,49 +126,58 @@ class cryptoMenu:
                                                 data.read_trades_to_hash())
 
     def asset_details_menu(self):
-        self.clear_screen()
-        print("\nFIND ASSET DETAILS")
-        print("------")
-        while True:
-            try:
-                symbol = str(input("Enter asset symbol (Bitcoin is BTC): "))
-                if len(symbol) > 2:
-                    asset_data = self.current_market.find_asset_details(symbol)
-                    if asset_data != None:
-                        asset_data.print_info()
-                    else:
-                        print("Asset symbol not found")
-                    break
-                print("Invalid symbol entered")
-            except Exception as e:
-                print("Error: ", e)
+        if self.current_market.has_asset_data() == False:
+                print("\n--LOAD ASSETS DATA FIRST--\n")
+        else:
+            self.clear_screen()
+            print("\nFIND ASSET DETAILS")
+            print("------")
+            while True:
+                try:
+                    symbol = str(input("Enter asset symbol (Bitcoin is BTC): "))
+                    if len(symbol) > 2:
+                        asset_data = self.current_market.find_asset_details(symbol)
+                        if asset_data != None:
+                            asset_data.print_info()
+                        else:
+                            print("Asset symbol not found")
+                        break
+                    print("Invalid symbol entered")
+                except Exception as e:
+                    print("Error: ", e)
         
     def trade_details_menu(self):
-        self.clear_screen()
-        print("\nFIND TRADE DETAILS")
-        print("------")
-        while True:
-            try:
-                symbol = str(input("Enter trades symbol (Bitcoin to Ethereum is BTCETH): "))
-                if len(symbol) > 2:
-                    print(symbol)
-                    trade_data = self.current_market.find_trade_details(symbol)
-                    if trade_data != None:
-                        trade_data.print_info()
-                    else:
-                        print("Trade symbol not found")
-                    break
-                print("Invalid symbol entered")
-            except Exception as e:
-                print("Error: ", e)
+        if self.current_market.has_trades_data() == False:
+                print("\n--LOAD TRADES DATA FIRST--\n")
+        else:       
+            self.clear_screen()
+            print("\nFIND TRADE DETAILS")
+            print("------")
+            while True:
+                try:
+                    symbol = str(input("Enter trades symbol (Bitcoin to Ethereum is BTCETH): "))
+                    if len(symbol) > 2:
+                        print(symbol)
+                        trade_data = self.current_market.find_trade_details(symbol)
+                        if trade_data != None:
+                            trade_data.print_info()
+                        else:
+                            print("Trade symbol not found")
+                        break
+                    print("Invalid symbol entered")
+                except Exception as e:
+                    print("Error: ", e)
 
     def asset_overview(self):
-        self.current_market.assets_ll_to_array()
-        self.clear_screen()
-        self.current_market.top_ten_by_market_cap()
-        self.current_market.top_ten_by_circulating()
-        self.current_market.top_ten_by_24_percent()
-        print("\n\n---SCROLL TO TOP TO VIEW ALL INFO---\n\n")
+        if self.current_market.has_asset_data() == False:
+                print("\n--LOAD ASSETS DATA FIRST--\n")
+        else:
+            self.current_market.assets_ll_to_array()
+            self.clear_screen()
+            self.current_market.top_ten_by_market_cap()
+            self.current_market.top_ten_by_circulating()
+            self.current_market.top_ten_by_24_percent()
+            print("\n\n---SCROLL TO TOP TO VIEW ALL INFO---\n\n")
 
     def trade_overview(self):
         if self.current_market.has_trades_data() == False:
@@ -188,17 +188,24 @@ class cryptoMenu:
             self.current_market.top_ten_price_change_percent()
             self.current_market.top_ten_volume_traded()
             self.current_market.top_ten_high_price()
+            print("\n\n---SCROLL TO TOP TO VIEW ALL INFO---\n\n")
+
+    def set_asset_filter(self):
+        pass
 
 
     def save_data_menu(self):
-        self.clear_screen()
-        print("SAVE DATA")
-        print("------")
-        name = str(input("Choose filename to save object as: "))
-        name = name + ".obj"
-        pickle_obj = Pickle_Menu(self.current_market)
-        pickle_obj.save(name)
-        print("\n---MARKET SAVED---\n")
+        if self.current_market.has_trades_data() == False:
+                print("\n--LOAD TRADES DATA FIRST--\n")
+        else:
+            self.clear_screen()
+            print("SAVE DATA")
+            print("------")
+            name = str(input("Choose filename to save object as: "))
+            name = name + ".obj"
+            pickle_obj = Pickle_Menu(self.current_market)
+            pickle_obj.save(name)
+            print("\n---MARKET SAVED---\n")
 
     def adjust_option_num(self, num):
         return num + 1
